4a02217fea0aaeb247a8875dd65f48a3e17d275e
Rick Van Overbeke <rick@rickvanoverbeke.be>
2025-06-01 00:04:59 +0200

    fixes

--- read_p1.py
+++ read_p1.py
@@ -29,10 +29,10 @@ class P1Reader():
 
     def add_logger(self):
         logger = logging.getLogger(__name__)
-        logger.setLevel(logging.debug)
+        logger.setLevel(logging.DEBUG)
 
         handler = logging.StreamHandler(sys.stdout)
-        handler.setLevel(logging.debug)
+        handler.setLevel(logging.DEBUG)
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         handler.setFormatter(formatter)
         logger.addHandler(handler)
